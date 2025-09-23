5fe31a46391e52b9f423d2bc39bb7a9bcb4f2b62
RishiTirpude19 <152504954+RishiTirpude19@users.noreply.github.com>
2025-09-23 10:51:26 +0530

    Update loginRoutes.js

--- backend/routes/loginRoutes.js
+++ backend/routes/loginRoutes.js
@@ -30,7 +30,7 @@ const msalConfig = {
     auth: {
         clientId: 'f33b06ab-53bf-49cf-b257-dc1188511391',
         authority: 'https://login.microsoftonline.com/850aa78d-94e1-4bc6-9cf3-8c11b530701c',
-        clientSecret: 'F6J8Q~tSsV4.LyzpsYOUnf2-WDwenXqkDFq6NcPY'
+        clientSecret: 'DUa8Q~nLueDDq~X5.8I~Y0G_4LIvvRlKuo6F0dkk'
     }
 };
 
